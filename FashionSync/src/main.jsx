5e4d67fe257dd8eac51fc606c6d4b1bc261965b2
TasnimBakli <tasneembaqli04@gmail.com>
2026-07-10 00:06:03 +0300

    Refactor frontend structure

--- FashionSync/src/main.jsx
+++ FashionSync/src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./styles/global.scss";
-import App from "./App";
+import "./frontend/styles/global.scss";
+import App from "./frontend/App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
